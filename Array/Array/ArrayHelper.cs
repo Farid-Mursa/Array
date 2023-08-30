979dbf4414ca6803e5abf55a96b6e5dfdc698a6f
Farid-Mursa <117182057+Farid-Mursa@users.noreply.github.com>
2023-08-30 17:10:05 +0400

    Add files via upload

---
+++ Array/Array/ArrayHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAddRemove
+{
+    internal static class ArrayHelper
+    {
+        public static string[] Add(ref string[] arr, string name)
+        {
+            
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[^1] = name;
+            return arr;
+        }
+        public static string[] Remove(ref string[] arr, string value)
+        {
+            int num = Array.IndexOf(arr, value);
+            if (num < 0) return arr;
+
+            for (int i = num; i <= arr.Length; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+
+            Array.Resize(ref arr, arr.Length - 1);
+
+            return arr;
+        }
+    }
+}
---
+++ Array/Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using ArrayAddRemove;
+
+namespace Arrayfasfas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] arr = { "Ferid","Salam","Saqol","Hello","NoHello" };
+            //ArrayHelper.Add(ref arr, "Pirivit");
+            //ArrayHelper.Add(ref arr, "Pokavet");
+            //ArrayHelper.Remove(ref arr, "Ferid");
+
+            foreach (string item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
